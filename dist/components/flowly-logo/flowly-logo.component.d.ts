cc109a24cbde42b3e9e3b9a3df4410006ae78b5f
Felipe Campos <felipecamposdev@gmail.com>
2025-03-12 12:10:21 -0300

    build

--- dist/components/flowly-logo/flowly-logo.component.d.ts
+++
@@ -1,6 +0,0 @@
-import { SVGProps } from 'react';
-type Props = {
-    width?: number;
-} & SVGProps<SVGSVGElement>;
-declare const SvgComponent: ({ width, ...props }: Props) => import("react/jsx-runtime").JSX.Element;
-export { SvgComponent as FlowlyLogo };
--- dist/components/left-drawer/left-drawer.component.d.ts
+++
@@ -1,4 +0,0 @@
-import { default as React } from 'react';
-export declare function LeftDrawer({ children, }: {
-    children: (isExpanded: boolean) => React.ReactNode;
-}): import("react/jsx-runtime").JSX.Element;
